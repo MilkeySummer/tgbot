f01a0f5b638c4e3d2a77ecd3f9335407000435ef
MilkeySummer <andrey222930@gmail.com>
2026-08-11 15:34:56 +0300

    Добавил SpendRepositoryTest

--- src/test/java/ru/skillfactorydemo/tgbot/repository/SpendRepositoryTest.java
+++ src/test/java/ru/skillfactorydemo/tgbot/repository/SpendRepositoryTest.java
@@ -1,4 +1,25 @@
 package ru.skillfactorydemo.tgbot.repository;
 
-public class SpendRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.skillfactorydemo.tgbot.entity.Spend;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class SpendRepositoryTest {
+
+    @Autowired
+    private SpendRepository spendRepository;
+
+    @Test
+    public void testRepo() {
+        //noinspection StatementWithEmptyBody
+        for (int i = 0; i < 10; i++, spendRepository.save(new Spend()));
+        final List<Spend> found = spendRepository.findAll();
+        assertEquals(10, found.size());
+    }
 }
